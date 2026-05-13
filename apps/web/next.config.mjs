6529149c62f114fb201c6f09a0f84543f460234c
Mohamed Magdy <mohamed.magdy@trianglz.com>
2026-05-13 16:36:04 +0300

    hardcoded backend api for free hosting problems

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,6 @@
-const API_URL = process.env.API_URL || "http://localhost:4000";
+const API_URL =
+  process.env.API_URL ||
+  (process.env.VERCEL ? "https://in-out-js-be-fe.onrender.com" : "http://localhost:4000");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
